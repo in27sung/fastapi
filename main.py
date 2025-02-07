2cb10bb3ce8c2bd41b96890958471ef2d679fafd
insung <in27sung@gmail.com>
2025-02-07 22:34:21 +0900

    Initialize FastAPI project

---
+++ main.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+##
+# Union is used to specify that the function can return either a str or an int
+# Decorator @app.get("/")
+# HTTP Method: GET
+# URL Path: "/"
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to FastAPI!!!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+# uvicorn main:app --reload
