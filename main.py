44e3ec5d9633f2baa8940321a3d1002a081dff36
insung <in27sung@gmail.com>
2025-02-08 18:31:06 +0900

    Initialize FastAPI project

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from typing import Union
+from typing import Union, Optional
 
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
+# uvicorn main:app --reload
 app = FastAPI()
+
+# BaseModel is used to specify the type of the request body
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = False
+    rating: Optional[int] = None
 
 ##
 # Union is used to specify that the function can return either a str or an int
@@ -17,4 +27,17 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-# uvicorn main:app --reload
+
+@app.post("/createposts")
+# def create_post(payload: dict = Body(...)):
+#     print(payload)
+#     return {"new_post": f"title {payload['title']}",
+#             "content": f"{payload['content']}",
+#             "message": "Post created successfully"}
+def create_post(post: Post):
+    # print(post)
+    print(post.model_dump())
+    return {"data": post}
+# title str, content str
+
+
